21e321efaf17177ba55d91790d5f5951ead66cfe
evanpurl <evan@purlcomputers.com>
2023-03-06 23:03:45 -0700

    fixed reload command.

--- cogs/commands.py
+++ cogs/commands.py
@@ -8,15 +8,17 @@ class scommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="squidreload", description="Command to reload cogs")
+    @commands.command(name="reload", description="Command to reload cogs")
     async def reload(self, ctx) -> None:
-        if await whohasaccess(ctx.message.author.id):
-            print(f"Syncing commands")
-            await self.bot.tree.sync()
-            await ctx.send(f"Commands synced")
-            print(f"Commands synced")
-        else:
-            await ctx.send(f"You can't run this command.")
+        tagged = ctx.message.mentions
+        if tagged[0].id == self.bot.user.id:
+            if await whohasaccess(ctx.message.author.id):
+                print(f"Syncing commands")
+                await self.bot.tree.sync()
+                await ctx.send(f"Commands synced")
+                print(f"Commands synced")
+            else:
+                await ctx.send(f"You can't run this command.")
 
 async def setup(bot):
     await bot.add_cog(scommands(bot))
